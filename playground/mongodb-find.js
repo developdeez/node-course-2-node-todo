bd23f8ce30e17e9352b05df0154b3ab59951bbe0
Cecil Carter <ccarter@HME.COM>
2018-03-19 21:30:21 -0700

    Initial commit

---
+++ playground/mongodb-find.js
@@ -0,0 +1,13 @@
+const {MongoClient,ObjectID}=require('mongodb');
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp',(err,db)=>{
+    if(err){
+        return console.log('Unable to connect to db.');
+    }
+    console.log('Connected to db');
+
+    db.collection('Users').find({name:'Ant'}).toArray().then((docs)=>{
+        console.log("users:");
+        console.log(JSON.stringify(docs,undefined,2));
+    });
+});
